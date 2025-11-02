e97f8e393ce39906f5a342a3339099ff551f41d9
Cian Jinks <cjinks99@gmail.com>
2025-11-02 22:36:09 +0000

    More setup

--- flake.nix
+++ flake.nix
@@ -16,16 +16,21 @@
         pkgs = import nixpkgs { inherit system; };
       in
       {
-        devShell = pkgs.mkShell.override { stdenv = pkgs.clang19Stdenv; } {
+        devShell = pkgs.mkShell {
           packages = [
             pkgs.bazel_7
             pkgs.bazel-buildtools
+            pkgs.gcc15
             # These go packages are just for VSCode / IDE
             # Bazel uses it's own Go toolchain declared in MODULE.bazel
             pkgs.go
             pkgs.gopls
             pkgs.delve
           ];
+          shellHook = ''
+            export CC=${pkgs.gcc15}/bin/gcc
+            export CXX=${pkgs.gcc15}/bin/g++
+          '';
         };
       }
     );
